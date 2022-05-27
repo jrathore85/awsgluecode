8c896a2c4506d41c125136984a99697b1bec7a81
data-engg-user1-at-460758253413 <>
2022-05-27 13:30:28 +0530

    added folders

---
+++ GlueScripts/etl - C1 - Copy.py
@@ -0,0 +1,9 @@
+from awsglue.context import GlueContext
+from awsglue.transforms import *
+from pyspark.context import SparkContext
+
+glueContext = GlueContext(SparkContext.getOrCreate())
+
+print('glueContext:', glueContext)
+print('glueContext2222222222222ggggggggxxxxxxxxxxxxxxxxxxxxxxxxxxx:', glueContext)
+
