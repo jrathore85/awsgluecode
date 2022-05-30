792cbd1b487874fe6818e69468f3e0477eaa9a91
data-engg-user1-at-460758253413 <>
2022-05-30 11:51:30 +0530

    addd

--- GlueScripts/etl.py
+++ GlueScripts/etl.py
@@ -5,5 +5,5 @@ from pyspark.context import SparkContext
 glueContext = GlueContext(SparkContext.getOrCreate())
 
 print('glueContext:', glueContext)
-print('glueContext  \\hhggjgh', glueContext)
+print('glueContext  \\hhggj   gh', glueContext)
 
