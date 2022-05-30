aa6026321be051fba30332d2152bcca541c801b7
data-engg-user1-at-460758253413 <>
2022-05-30 18:20:32 +0530

    addddd

--- GlueScripts/etl.py
+++ GlueScripts/etl.py
@@ -1,9 +1,9 @@
-from awsglue.context import GlueContext
+cfrom awsglue.context import GlueContext
 from awsglue.transforms import *
 from pyspark.context import SparkContext
 
 glueContext = GlueContext(SparkContext.getOrCreate())
 
 print('glueContext:', glueContext)
-print('glueContext  \\hhggj   jkkkkkkkkggkj                          jkc cccgh', glueContext)
+print('glueContext  \\hhggj   jkkkkkkkkggkj      c                    jkc cccgh', glueContext)
 
