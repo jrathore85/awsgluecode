4b6382f5f1f3f5cc828efcbb569082ec57655df7
data-engg-user1-at-460758253413 <>
2022-06-09 15:05:20 +0530

    modifyed at 9 June

--- GlueScripts/etl.py
+++ GlueScripts/etl.py
@@ -1,9 +1,13 @@
 from awsglue.context import GlueContext
 from awsglue.transforms import *
 from pyspark.context import SparkContext
+import time
 
 glueContext = GlueContext(SparkContext.getOrCreate())
 
 print('glueContext:', glueContext)
 print('this is final code at 7fffffffffffffff June', glueContext)
+print('Before sleep')
+time.sleep(300) 
+print('After sleep')
 
